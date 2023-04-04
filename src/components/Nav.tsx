962698c3f49a88d3304e3ef40b85426137e981d3
ryan anderson <ryanmanderson1986@gmail.com>
2023-04-04 18:04:32 -0500

    add: theme context provider component

---
+++ src/components/Nav.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Nav = props => {
+  return (
+    <>
+        <nav>
+            <div className='container'>
+                Container
+            </div>
+        </nav>
+    </>
+  )
+}
+
+Nav.propTypes = {}
+
+export default Nav
